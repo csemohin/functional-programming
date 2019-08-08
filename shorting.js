c174fce0602e55c5d47a01e9c3dc9f9381b7cac1
csemohin <csemohin@gmail.com>
2019-08-09 01:05:10 +0600

    Make filter function

---
+++ shorting.js
@@ -0,0 +1,23 @@
+var arr = [1,2,3,4,5,6,6];
+//filter function
+// var srt = arr.filter(el => {
+//     return el % 2 ===0;     //return always true or false
+// })
+// console.log(srt);
+
+
+// make a filter function
+function myFilter(anyArr,cb){
+    var newArr = [];
+    for(var i = 0; i < anyArr.length; i++){
+        if( cb(anyArr[i], i, anyArr)){
+            newArr.push(anyArr[i])
+        }
+    }
+    return newArr;
+}
+
+console.log(myFilter(arr, function(el){
+    return el % 2 === 1;
+}));
+
